d5943db7f6bca21433b0f4cd5df64c9c76845c8d
Jesus Cangalaya <jesuscangalaya1@gmail.com>
2023-08-20 22:19:16 -0600

    railway deplor ...

---
+++ src/main/java/com/idat/pe/service/PurchaseService.java
@@ -0,0 +1,27 @@
+package com.idat.pe.service;
+
+import com.idat.pe.dto.request.PurchaseRequest;
+import com.idat.pe.dto.response.FlightResponse;
+import com.idat.pe.dto.response.PurchaseResponse;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface PurchaseService {
+
+    void designBoleto(FlightResponse flightResponse);
+
+    List<PurchaseResponse> getDeletedPurchases();
+
+    void restorePurchase(Long id);
+
+    void deletePurchase(Long id);
+
+    void purchaseFlight(PurchaseRequest purchaseRequest);
+
+    List<PurchaseResponse> getCustomerPurchases();
+
+    ResponseEntity<Resource> exportInvoice(Long id);
+
+}
